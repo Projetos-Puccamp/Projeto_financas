b8eb7a39c22109d2beaf5a7d6dba08749966136f
MiguelCamposPinto <migpinto2002@gmail.com>
2023-10-03 19:07:57 -0300

    Create BD.sql

---
+++ BD.sql
@@ -0,0 +1,16 @@
+create database Financas;
+CREATE TABLE Conta (
+    ContaID INT PRIMARY KEY AUTO_INCREMENT,
+    EmailLogin VARCHAR(255) NOT NULL,
+    Senha VARCHAR(255) NOT NULL,
+    Nome VARCHAR(255),
+    DataNascimento DATE
+);
+CREATE TABLE UsuarioSaldo (
+    UsuarioSaldoID INT PRIMARY KEY AUTO_INCREMENT,
+    ContaID INT,
+    Saldo DECIMAL(10, 2),
+    TipoConta VARCHAR(255),
+    Cartao VARCHAR(255),
+    FOREIGN KEY (ContaID) REFERENCES Conta(ContaID)
+);
