d1dc41af7efa2d7334bc7b1503c81146bd7a811b
Farlos123Marlos <92122616+Farlos123Marlos@users.noreply.github.com>
2023-10-12 22:07:25 -0300

    add switch para lembrar conta

--- APP-Financas/LoginScreen.js
+++ APP-Financas/LoginScreen.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native'; // Import the useNavigation hook
+import { Switch } from 'react-native';
 
 function LoginScreen() {
   const navigation = useNavigation(); // Get the navigation object
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [rememberAccount, setRememberAccount] = useState(false);
 
 
   const handleLogin = () => {
@@ -50,36 +52,45 @@ function LoginScreen() {
 
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.headerText}>Login</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome de usuário"
-        onChangeText={(text) => setUsername(text)}
-        value={username}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Senha"
-        secureTextEntry
-        onChangeText={(text) => setPassword(text)}
-        value={password}
-      />
-      <TouchableOpacity
-        style={styles.button}
-        onPress={handleLogin}
-      >
-        <Text style={styles.buttonText}>Entrar</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.signupButton}
-        onPress={handleSignupPress}
-      >
-        <Text style={styles.signupButtonText}>Cadastre-se</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Login</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome de usuário"
+          onChangeText={(text) => setUsername(text)}
+          value={username}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          secureTextEntry
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+        />
+
+        <View style={styles.switchContainer}>
+          <Text style={styles.switchLabel}>Lembrar Minha Conta</Text>
+          <Switch
+            value={rememberAccount}
+            onValueChange={(value) => setRememberAccount(value)}
+          />
+        </View>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleLogin}
+        >
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.signupButton}
+          onPress={handleSignupPress}
+        >
+          <Text style={styles.signupButtonText}>Cadastre-se</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
@@ -121,6 +132,15 @@ const styles = StyleSheet.create({
     color: '#007AFF',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 10,
+  },
+  switchLabel: {
+    fontSize: 16,
   },
 });
 
